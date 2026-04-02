1c3f439157865811c6daf8acb3858d5e6fc60721
anishan3213-design <anishan3213@gmail.com>
2026-04-02 01:52:36 -0400

    Initial Commit

---
+++ Minutes Matter/Minutes_MatterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minutes_MatterApp.swift
+//  Minutes Matter
+//
+//  Created by Akhi Chappidi on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Minutes_MatterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
